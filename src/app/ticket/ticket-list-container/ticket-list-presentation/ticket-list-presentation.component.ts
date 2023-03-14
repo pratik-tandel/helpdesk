db07c3009a17d2061cb279495cf60019460f55a3
PratikTandel <pratik.tandel@1rivet.com>
2023-03-14 17:57:53 +0530

    feat: export tickets in xls

--- src/app/ticket/ticket-list-container/ticket-list-presentation/ticket-list-presentation.component.ts
+++ src/app/ticket/ticket-list-container/ticket-list-presentation/ticket-list-presentation.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import * as XLSX from 'xlsx';
 import { Ticket } from '../../ticket.model';
 
 @Component({
@@ -18,6 +19,8 @@ export class TicketListPresentationComponent {
 
   @Output() public deleteTicket: EventEmitter<number>;
 
+  public fileName: string = 'Tickets.xls';
+
   private _ticketList!: Ticket[] | null;
 
   constructor() {
@@ -26,5 +29,18 @@ export class TicketListPresentationComponent {
 
   onDelete(ticketId: number) {
     this.deleteTicket.emit(ticketId);
+  }
+
+  exportExcel(): void {
+    /* table id */
+    let element = document.getElementById('ticket-table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Tickets');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
   }
 }
