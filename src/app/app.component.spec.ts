7bad3af5758c456ddf8da41aacb5c7b7fa4bafb1
PratikTandel <pratik.tandel@1rivet.com>
2023-04-13 13:24:26 +0530

    feat: add testcases for search component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lms'`, () => {
+  it(`should have as title 'helpdesk'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('lms');
+    expect(app.title).toEqual('helpdesk');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('lms app is running!');
-  });
 });
--- src/app/shared/components/search/search.component.spec.ts
+++ src/app/shared/components/search/search.component.spec.ts
@@ -1,16 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-describe('SearchComponent', () => {
+fdescribe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [SearchComponent],
+      imports: [FormsModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
@@ -20,4 +23,17 @@ describe('SearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  xit('should call onSearch on keyup', () => {
+    const onSearchMock = spyOn(component, 'onSearch');
+    fixture.debugElement.query(By.css('#search')).triggerEventHandler('keyup', null);
+    expect(onSearchMock).toHaveBeenCalled();
+  });
+
+  it('should emit search term on keyup', () => {
+    const searchEmitMock = spyOn(component.search, 'emit');
+    fixture.debugElement.query(By.css('#search')).triggerEventHandler('keyup', null);
+    expect(searchEmitMock).toHaveBeenCalled();
+  });
+
 });
