7f614bc6098f7c180a36c445afabac33940bf9a7
PratikTandel <pratik.tandel@1rivet.com>
2023-04-17 12:09:03 +0530

    feat: add testcases for auth service

--- factory/api-data.ts
+++ factory/api-data.ts
@@ -1,63 +1,71 @@
 export const TICKETS = [
     {
-        "category": "Infrastructure",
-        "subject": "Test",
-        "message": "awe",
-        "priority": "ae",
-        "assignee": "Neel Pardeshi",
-        "status": "New",
-        "raisedBy": "Pratik Tandel",
-        "updated": "2023-03-13T12:12:15.633Z",
-        "id": 1
+        category: 'Infrastructure',
+        subject: 'Test',
+        message: 'awe',
+        priority: 'ae',
+        assignee: 'Neel Pardeshi',
+        status: 'New',
+        raisedBy: 'Pratik Tandel',
+        updated: '2023-03-13T12:12:15.633Z',
+        id: 1
     },
     {
-        "category": "1R University",
-        "subject": "Test",
-        "message": "TEst",
-        "priority": "TEStse",
-        "assignee": "Neel Pardeshi",
-        "status": "New",
-        "raisedBy": "Pratik Tandel",
-        "updated": "2023-03-13T12:12:39.111Z",
-        "id": 2
+        category: '1R University',
+        subject: 'Test',
+        message: 'TEst',
+        priority: 'TEStse',
+        assignee: 'Neel Pardeshi',
+        status: 'New',
+        raisedBy: 'Pratik Tandel',
+        updated: '2023-03-13T12:12:39.111Z',
+        id: 2
     }
 ]
 
 export const TICKET_FORM = {
-    "category": "1R University",
-    "subject": "Test",
-    "message": "TEst",
-    "priority": "TEStse",
-    "assignee": "Neel Pardeshi",
-    "status": "New",
-    "raisedBy": "Pratik Tandel",
-    "updated": "2023-03-13T12:12:39.111Z",
-    "id": 2
+    category: '1R University',
+    subject: 'Test',
+    message: 'TEst',
+    priority: 'TEStse',
+    assignee: 'Neel Pardeshi',
+    status: 'New',
+    raisedBy: 'Pratik Tandel',
+    updated: '2023-03-13T12:12:39.111Z',
+    id: 2
 }
 
 export const CATEGORIES = [
     {
-        "id": 1,
-        "categoryName": "1R University"
+        id: 1,
+        categoryName: '1R University'
     },
     {
-        "id": 2,
-        "categoryName": "Account"
+        id: 2,
+        categoryName: 'Account'
     },
     {
-        "id": 3,
-        "categoryName": "Devops"
+        id: 3,
+        categoryName: 'Devops'
     },
     {
-        "id": 4,
-        "categoryName": "HR"
+        id: 4,
+        categoryName: 'HR'
     },
     {
-        "id": 5,
-        "categoryName": "Infrastructure"
+        id: 5,
+        categoryName: 'Infrastructure'
     },
     {
-        "id": 6,
-        "categoryName": "Office Administration"
+        id: 6,
+        categoryName: 'Office Administration'
     }
 ]
+
+export const USER_DETAILS = {
+    userName: 'pratiktandel@gmail.com',
+    password: '123',
+    name: 'Pratik',
+    lastName: 'Tandel',
+    id: 1
+}
