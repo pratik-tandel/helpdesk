1b65fea372b4cd18d13b64284f9cb0f7139d1efe
PratikTandel <pratik.tandel@1rivet.com>
2023-04-17 11:26:02 +0530

    feat: add testcases for ticket-form presenter

--- src/app/ticket/ticket-form-container/ticket-form-presenter/ticket-form-presenter.service.spec.ts
+++ src/app/ticket/ticket-form-container/ticket-form-presenter/ticket-form-presenter.service.spec.ts
@@ -1,16 +1,45 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { TestBed } from '@angular/core/testing';
+import { TicketFormPresenterService } from "./ticket-form-presenter.service";
+import { ASSIGNEE, CATEGORY } from "src/app/core/utility/constant";
 
-import { TicketFormPresenterService } from './ticket-form-presenter.service';
-
-describe('TicketFormPresenterService', () => {
+describe("TicketFormPresenterService", () => {
   let service: TicketFormPresenterService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        TicketFormPresenterService
+      ]
+    });
     service = TestBed.inject(TicketFormPresenterService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('setAssignee should return assignee', () => {
+    let assignee = service.setAssignee(CATEGORY.UNIVERSITY);
+    expect(assignee).toBe(ASSIGNEE.UNIVERSITY);
+
+    assignee = service.setAssignee(CATEGORY.ACCOUNT);
+    expect(assignee).toBe(ASSIGNEE.ACCOUNT);
+
+    assignee = service.setAssignee(CATEGORY.DEVOPS);
+    expect(assignee).toBe(ASSIGNEE.DEVOPS);
+
+    assignee = service.setAssignee(CATEGORY.HR);
+    expect(assignee).toBe(ASSIGNEE.HR);
+
+    assignee = service.setAssignee(CATEGORY.INFRASTRUCTURE);
+    expect(assignee).toBe(ASSIGNEE.INFRASTRUCTURE);
+
+    assignee = service.setAssignee(CATEGORY.OFFICE_ADMINSTRATION);
+    expect(assignee).toBe(ASSIGNEE.OFFICE_ADMINSTRATION);
+
+    assignee = service.setAssignee('Others');
+    expect(assignee).toBe(ASSIGNEE.DEFAULT);
   });
 });
